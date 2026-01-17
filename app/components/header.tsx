116b5e02e222bcb8595a7e1a96a3b9229d02524e
Lisa-Manresa <lisa.manresa@roundingwell.com>
2026-01-17 14:16:04 -0800

    Added component-specific css files

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Link from "next/link";
 import { usePathname } from 'next/navigation';
+import "./header.css";
 
 export default function Header() {
     const pathname = usePathname();
