7e5d000b7c435c3271d32c5060f25f72a99d405b
Arctyx-0 <superderp12362@gmail.com>
2026-01-16 17:18:09 -0800

    creation

---
+++ app/nextMatch/page.tsx
@@ -0,0 +1,8 @@
+
+export default function nextMatch() {
+  return (
+    <div className="flex min-h-screen font-sans bg-green-950/90">
+      <p className="text-3xl">Next Match</p>
+    </div>
+  );
+}
---
+++ app/teams/page.tsx
@@ -0,0 +1,7 @@
+export default function teams() {
+  return (
+    <div className="flex min-h-screen bg-zinc-50 font-sans dark:bg-black">
+      <p className="text-3xl">Teams</p>
+    </div>
+  );
+}
