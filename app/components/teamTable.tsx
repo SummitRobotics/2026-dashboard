11d61a738cca8d6bc5105bb255d55f3ef7f2b165
Arctyx-0 <superderp12362@gmail.com>
2026-01-19 15:35:27 -0800

    ranking, change sorting defaults

--- app/components/teamTable.tsx
+++ app/components/teamTable.tsx
@@ -2,11 +2,13 @@ import Link from "next/link";
 
 interface TeamData {
   teamNumber: number;
+  teamName: string;
   epa: number;
   autoEpa: number;
   teleEpa: number;
   endEpa: number;
   opr: number;
+  rank: number;
 }
 
 async function getCompData(eventKey: string): Promise<TeamData[]> {
@@ -31,11 +33,13 @@ async function getCompData(eventKey: string): Promise<TeamData[]> {
 
     const combined: TeamData[] = statData.map((entry: any) => ({
       teamNumber: entry.team,
+      teamName: entry.team_name,
       epa: entry.epa?.breakdown?.total_points ?? 0,
       autoEpa: entry.epa?.breakdown?.auto_points ?? 0,
       teleEpa: entry.epa?.breakdown?.teleop_points ?? 0,
       endEpa: entry.epa?.breakdown?.endgame_points ?? 0,
-      opr: oprs[`frc${entry.team}`] ?? 0
+      opr: oprs[`frc${entry.team}`] ?? 0,
+      rank: entry.record?.qual?.rank ?? 0
     }));
 
     return combined;
@@ -63,42 +67,52 @@ export default async function EventPage({
   });
 
   const getSortUrl = (key: string) => {
-    const newOrder = sortKey === key && !isAsc ? 'asc' : 'desc';
+    const newOrder = sortKey === key && !isAsc || key === 'rank' || key === "teamNumber" || key === 'teamName' ? 'asc' : 'desc';
     return `?sort=${key}&order=${newOrder}`;
   };
 
   return (
-    <table className="w-full border-collapse border border-slate-800 text-white bg-black">
+    <table className="w-max border-collapse border border-slate-800 text-white bg-black">
       <thead className="bg-slate-900 uppercase text-xs font-bold">
         <tr>
-          <th className="p-3 border border-slate-800 text-left">
+          <th className="p-3 border border-slate-800 text-left w-px whitespace-nowrap">
             <Link href={getSortUrl('teamNumber')} className=" flex items-center gap-1">
               Team {sortKey === 'teamNumber' ? (isAsc ? '▲' : '▼') : '↕'}
             </Link>
           </th>
           <th className="p-3 border border-slate-800 text-left">
+            <Link href={getSortUrl('teamName')} className=" flex items-center gap-1">
+              Team Name {sortKey === 'teamName' ? (isAsc ? '▲' : '▼') : '↕'}
+            </Link>
+          </th>
+          <th className="p-3 border border-slate-800 text-left w-px whitespace-nowrap">
             <Link href={getSortUrl('epa')} className="flex items-center gap-1">
               EPA {sortKey === 'epa' ? (isAsc ? '▲' : '▼') : '↕'}
             </Link>
           </th>
-          <th className="p-3 border border-slate-800 text-left">
+          <th className="p-3 border border-slate-800 text-left w-px whitespace-nowrap">
             <Link href={getSortUrl('autoEpa')} className="flex items-center gap-1">
                 Auto EPA {sortKey === "autoEpa" ? (isAsc ? '▲' : '▼') : '↕'}
             </Link>
           </th>
-          <th className="p-3 border border-slate-800 text-left">
+          <th className="p-3 border border-slate-800 text-left w-px whitespace-nowrap">
             <Link href={getSortUrl('teleEpa')} className="flex items-center gap-1">
                 Teleop EPA {sortKey === "teleEpa" ? (isAsc ? '▲' : '▼') : '↕'}
             </Link>
           </th>
-          <th className="p-3 border border-slate-800 text-left">
+          <th className="p-3 border border-slate-800 text-left w-px whitespace-nowrap">
             <Link href={getSortUrl('endEpa')} className="flex items-center gap-1">
                 Endgame EPA {sortKey === "endEpa" ? (isAsc ? '▲' : '▼') : '↕'}
             </Link>
           </th>
-          <th className="p-3 border border-slate-800 text-left">
+          <th className="p-3 border border-slate-800 text-left w-px whitespace-nowrap">
             <Link href={getSortUrl('opr')} className="flex items-center gap-1">
               OPR {sortKey === 'opr' ? (isAsc ? '▲' : '▼') : '↕'}
+            </Link>
+          </th>
+          <th className="p-3 border border-slate-800 text-left w-px whitespace-nowrap">
+            <Link href={getSortUrl('rank')} className="flex items-center gap-1">
+              Rank {sortKey === 'rank' ? (isAsc ? '▲' : '▼') : '↕'}
             </Link>
           </th>
         </tr>
@@ -106,12 +120,14 @@ export default async function EventPage({
       <tbody>
         {sortedTeams.map((team) => (
           <tr key={team.teamNumber}>
-            <td className="p-3 border border-slate-800 font-bold">{team.teamNumber}</td>
+            <td className="p-3 border border-slate-800 font-bold font-mono">{team.teamNumber}</td>
+            <td className="p-3 border border-slate-800 text-orange-400 font-mono">{team.teamName}</td>
             <td className="p-3 border border-slate-800 text-purple-400 font-mono">{team.epa.toFixed(1)}</td>
             <td className="p-3 border border-slate-800 text-green-400 font-mono">{team.autoEpa.toFixed(1)}</td>
             <td className="p-3 border border-slate-800 text-blue-400 font-mono">{team.teleEpa.toFixed(1)}</td>
             <td className="p-3 border border-slate-800 text-red-400 font-mono">{team.endEpa.toFixed(1)}</td>
-            <td className="p-3 border border-slate-800 text-yellow-400 font-mono">{team.opr.toFixed(1)}</td>
+            <td className="p-3 border border-slate-800 text-pink-400 font-mono">{team.opr.toFixed(1)}</td>
+            <td className="p-3 border border-slate-800 text-yellow-400 font-mono">{team.rank}</td>
           </tr>
         ))}
       </tbody>
