8f8604ba9a0fabf9205d7033b58295112c02a1dd
Lisa-Manresa <lisa.manresa@roundingwell.com>
2026-03-05 16:57:24 -0800

    header styling fix

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -19,8 +19,8 @@ export default function Header() {
     };
 
     return (
-        <header className="bg-white dark:bg-black p-4 shadow-md">
-            <nav id="mainNav" className="flex flex-wrap items-center justify-between w-full py-4 md:py-0 text-lg">
+        <header className="bg-black p-4 shadow-md">
+            <nav id="mainNav" className="flex flex-wrap items-center justify-between w-full md:py-0 text-lg">
                 <h1 className="font-chaos text-chaos text-left uppercase">
                     Chaos Theory
                 </h1>
@@ -29,15 +29,15 @@ export default function Header() {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
 
-                <div className="hidden w-full md:flex md:items-center md:w-auto" id="menu">
-                    <ul className="pt-4 text-base md:flex md:justify-between md:pt-0">
+                <div className="hidden w-full md:block md:w-auto" id="menu">
+                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 space-y-4 md:flex-row md:space-x-4 md:space-y-0 md:mt-0 items-center">
                         {links.map((link) => {
                             const isActive = pathname === link.href;
                             return (
                                 <li key={link.href} className="ml-2">
                                     <Link
                                         href={link.href}
-                                        className={`rounded-full py-2 px-4 text-black border-1 border-summit-grey-100 hover:border-chaos-300 [&.active]:border-chaos-300 bg-white hover:bg-chaos [&.active]:bg-chaos ${isActive ? 'active' : ''}`}
+                                        className={`inline-block rounded-full py-2 px-4 text-black border-1 border-summit-grey-100 hover:border-chaos-300 [&.active]:border-chaos-300 bg-white hover:bg-chaos [&.active]:bg-chaos ${isActive ? 'active' : ''}`}
                                     >
                                         {link.name}
                                     </Link>
